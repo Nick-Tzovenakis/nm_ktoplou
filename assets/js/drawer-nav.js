b4627001d167b4159b5a98e90c996a18fcf41c24
Nick-Tzovenakis <59684504+Nick-Tzovenakis@users.noreply.github.com>
2021-06-07 16:14:36 +0300

    index.html - Marina's review

--- assets/js/drawer-nav.js
+++ assets/js/drawer-nav.js
@@ -1,13 +1,15 @@
-const navToggle = document.getElementById("navToggle");
-const navCloseBtn = document.getElementById("navCloseBtn");
-const drawer = document.getElementById("drawer");
+window.addEventListener('DOMContentLoaded', (event) => {
+    const navToggle = document.getElementById("navToggle");
+    const navCloseBtn = document.getElementById("navCloseBtn");
+    const drawer = document.getElementById("drawer");
 
-navToggle.addEventListener("click", (e) => {
-    drawer.classList.add("nav-opened");
-    e.preventDefault();
-});
+    navToggle.addEventListener("click", (e) => {
+        drawer.classList.add("nav-opened");
+        e.preventDefault();
+    });
 
-navCloseBtn.addEventListener("click", (e) => {
-    drawer.classList.remove("nav-opened");
-    e.preventDefault();
+    navCloseBtn.addEventListener("click", (e) => {
+        drawer.classList.remove("nav-opened");
+        e.preventDefault();
+    });
 });
